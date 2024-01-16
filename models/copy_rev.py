7353e9a66ae6e7fd54f0b99d24576367343e7782
Oigbochie Blessed <eprince1800@gmail.com>
2024-01-16 05:50:04 -0800

    debbuged the review.py Review class

---
+++ models/copy_rev.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Review module for the HBNB project 
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+from models import place
+
+Base = declarative_base()
+
+class Review(BaseModel, Base):
+    """ Review class to store review information 
+    """
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id', nullable=False))
+    text = Column(String(1024), nullable=False)
