3d3795c8d534ec213509ea64ff8efd97e569128f
josephorokpo <josephorokpo@gmail.com>
2024-02-15 04:08:15 -0800

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,5 +32,4 @@ def is_number(n):
     return '{} is a number'.format(n)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
